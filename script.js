e5e97d4df81767a16caf8ebfa3e711bcad86a8cc
Ste-nandes <108842254+Ste-nandes@users.noreply.github.com>
2022-07-15 22:47:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+
+}, 2000)
+
+
+function nextImage(){
+    count++;
+    if(count>4){
+        count = 1;
+    }
+
+
+document.getElementById9("radio"+count).checked = true;
+
+}
